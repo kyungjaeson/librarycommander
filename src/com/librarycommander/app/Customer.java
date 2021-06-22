7e1a6db94bc7c858b21dedff7ccd024e5a2ecc22
Stephen Yeboah <stephenoyeboah@gmail.com>
2021-06-22 15:31:16 -0700

    Add the implementation for to Customer Class

--- src/com/librarycommander/app/Customer.java
+++ src/com/librarycommander/app/Customer.java
@@ -1,9 +1,14 @@
 package com.librarycommander.app;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Customer {
 
     private String name;
     private int id;
+    private List<Item> itemInPossession = new LinkedList<>();
+    private Library library =new Library();
 
     public Customer(String name){
         this.name = name;
@@ -12,6 +17,22 @@ public class Customer {
         this.name = name;
         this.id = id;
     }
+    public void checkOutItem(){
+
+    }
+    public  void checkInItem(){
+
+    }
+
+    public List<Item> search(String keyWord){
+        return null;
+    }
+    public  void renewItem(){
+
+    }
+    public void reserveItem(){
+
+    }
 
     public String getName() {
         return name;
@@ -19,5 +40,9 @@ public class Customer {
 
     public int getId() {
         return id;
+    }
+
+    public List<Item> getItemInPossession() {
+        return itemInPossession;
     }
 }
