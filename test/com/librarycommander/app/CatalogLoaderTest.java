b8acfb12cec976bd40f113e6eaf99e21f2d1807d
Stephen Yeboah <stephenoyeboah@gmail.com>
2021-06-23 12:34:29 -0700

    added test methods for load and write items to the catalogue.txt

--- test/com/librarycommander/app/CatalogLoaderTest.java
+++ test/com/librarycommander/app/CatalogLoaderTest.java
@@ -1,12 +1,19 @@
 package com.librarycommander.app;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CatalogLoaderTest extends TestCase {
 
+    @Test
     public void testLoadItemsFromFile() {
+        CatalogLoader loader=new CatalogLoader();
+        assertNotNull(loader.loadItemsFromFile());
     }
 
-    public void testWriteItemsToFile() {
+    @Test
+    public void testWriteItemsToFile_shouldReturnTrue_whenItemsSuccessfullyWriteToFile() {
+        assertTrue(new CatalogLoader().writeItemsToFile(new CatalogLoader().loadItemsFromFile()));
     }
 }
